83820d8442a5386b8dfb6dbc7298519ef173a725
gsbarr <gabriel.s.barreiro@gmail.com>
2024-08-07 20:43:57 -0300

    sad

--- js/Noticias.js
+++ js/Noticias.js
@@ -5,12 +5,15 @@ async function cargar_noticias(){
         , {
             method: "GET", // *GET, POST, PUT, DELETE, etc.    
             headers: {
+                'Accept': 'application/json',
                 "Content-Type": "application/json",
-            },
-            mode: "no-cors"
+            }
         });
 
-    datos = respuesta.json(); 
+    datos = await respuesta.json(); 
+
+
+    //datos = `[{"_id":"667ca5e2cb1799a0fe91c011","titulo":"se contrató a un canguro como secretario","subtitulo":"es muy bueno en su trabajo","texto":"En el día de ayer hubo una lciitación en la cual concursaron un canguro, un tigre y un elefante. Casi gana el elefante pero se olvidó un colmillo en su casa.","fechahora":"24/07/26 17:55hs"},{"_id":"667cacbe48071f1a90ce56e0","titulo":"se contrató a un canguro como secretario","subtitulo":"es muy bueno en su trabajo","texto":"En el día de ayer hubo una lciitación en la cual concursaron un canguro, un tigre y un elefante. Casi gana el elefante pero se olvidó un colmillo en su casa.","fechahora":"24/07/26 17:55hs"},{"_id":"667cacc248071f1a90ce56e1","titulo":"se contrató a un canguro como secretario","subtitulo":"es muy bueno en su trabajo","texto":"En el día de ayer hubo una lciitación en la cual concursaron un canguro, un tigre y un elefante. Casi gana el elefante pero se olvidó un colmillo en su casa.","fechahora":"24/07/26 17:55hs"},{"_id":"66b16159134105ec643de902","titulo":"hola","texto":"asdsada"}]`;
 
     console.log(datos);
 
