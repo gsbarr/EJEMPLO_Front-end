5d6e753f8f5a0eba0e30dd1b0209fbc25816c995
gsbarr <gabriel.s.barreiro@gmail.com>
2024-11-06 16:58:02 -0300

    asd

--- js/Noticias.js
+++ js/Noticias.js
@@ -9,33 +9,48 @@ async function listar_noticias(){
                 "Content-Type": "application/json",
             }
         });
-
+    
+    
     datos = await respuesta.json(); 
-
-
+    
+    
     //datos = `[{"_id":"667ca5e2cb1799a0fe91c011","titulo":"se contrató a un canguro como secretario","subtitulo":"es muy bueno en su trabajo","texto":"En el día de ayer hubo una lciitación en la cual concursaron un canguro, un tigre y un elefante. Casi gana el elefante pero se olvidó un colmillo en su casa.","fechahora":"24/07/26 17:55hs"},{"_id":"667cacbe48071f1a90ce56e0","titulo":"se contrató a un canguro como secretario","subtitulo":"es muy bueno en su trabajo","texto":"En el día de ayer hubo una lciitación en la cual concursaron un canguro, un tigre y un elefante. Casi gana el elefante pero se olvidó un colmillo en su casa.","fechahora":"24/07/26 17:55hs"},{"_id":"667cacc248071f1a90ce56e1","titulo":"se contrató a un canguro como secretario","subtitulo":"es muy bueno en su trabajo","texto":"En el día de ayer hubo una lciitación en la cual concursaron un canguro, un tigre y un elefante. Casi gana el elefante pero se olvidó un colmillo en su casa.","fechahora":"24/07/26 17:55hs"},{"_id":"66b16159134105ec643de902","titulo":"hola","texto":"asdsada"}]`;
 
     console.log(datos);
 
+    if (respuesta.status >= 200 && respuesta.status < 300){
+        datos.forEach(noti => {
 
-    datos.forEach(noti => {
-
-        nuevaNoticia = `<div class="row">
-                <div class="col s12">
-                    <div class="card-panel noticias">
-                        <h6 class="fecha">${noti.fechahora}</h6>
-                        <h2 class="titulo"> ${noti.titulo}</h2>
-                        <span class="texto">${noti.texto}
-                        </span>
+            nuevaNoticia = `<div class="row">
+                    <div class="col s12">
+                        <div class="card-panel noticias">
+                            <h6 class="fecha">${noti.fechahora}</h6>
+                            <h2 class="titulo"> ${noti.titulo}</h2>
+                            <span class="texto">${noti.texto}
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
+            `;
+    
+            seccionNoticias = document.querySelector(".SeccionNoticias");
+            seccionNoticias.innerHTML += nuevaNoticia;
+    
+        });
+    }
+    else{
+        console.log("código de respuesta: " + respuesta.status);
+
+        div =  `<div class="row">
+                    <div clas="col s12">
+                        <div class="card-panel noticias">
+                        <h6> NO ESTÁ AUTORIZADO A VER LOS DATOS </h6>
         `;
 
         seccionNoticias = document.querySelector(".SeccionNoticias");
-        seccionNoticias.innerHTML += nuevaNoticia;
-
-    });
+        seccionNoticias.innerHTML += div;
+    }
+    
 }
 
 
