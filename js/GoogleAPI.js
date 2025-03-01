4ba394df063e7e7bdc849fcf6a60d8cf32349844
gsbarr <gabriel.s.barreiro@gmail.com>
2025-02-28 22:39:46 -0300

    j

---
+++ js/GoogleAPI.js
@@ -0,0 +1,28 @@
+function decodeJwtResponse(token) {
+  let base64Url = token.split('.')[1];
+  let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
+function handleCredentialResponse(response) {
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+}
+
+window.onload = function () {
+google.accounts.id.initialize({
+client_id: '767201695177-mpk458qll979p5od35jr240v71m4rt42.apps.googleusercontent.com',
+callback: handleCredentialResponse
+});
+google.accounts.id.prompt();
+};
